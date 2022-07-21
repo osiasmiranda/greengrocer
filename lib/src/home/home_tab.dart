1eacd02def7dfef32c435b33c814d5d7138d4c83
Osias Miranda <osiasmiranda@gmail.com>
2022-07-21 18:21:50 -0400

    chore: adicinado Badge e campo de pesquisa

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 import '../config/custom_colors.dart';
@@ -37,16 +38,59 @@ class HomeTab extends StatelessWidget {
           ),
         ),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            color: CustomColors.customSwatchColor,
-            onPressed: () {},
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, right: 16.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                badgeColor: CustomColors.customContrastColor,
+                badgeContent: const Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+            ),
           ),
         ],
       ),
-      //Campo de Pesquisa
 
-      body: Container(),
+      body: Column(
+        children: [
+          //Campo de Pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  isDense: true,
+                  hintText: 'Pesquise aqui...',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade400,
+                    fontSize: 14,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: CustomColors.customContrastColor,
+                    size: 21,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(60),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  )),
+            ),
+          ),
+        ],
+      ),
       //Categorias
       //grid
     );
