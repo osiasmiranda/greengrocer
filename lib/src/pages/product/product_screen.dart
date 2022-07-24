634699eb7f654df221510300a290ec1600577fa7
Osias Miranda <osiasmiranda@gmail.com>
2022-07-23 21:54:59 -0400

    chore: adicionando dados do container

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../../config/custom_colors.dart';
 import '../../model/item_model.dart';
+import '../../services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
   final ItemModel item;
-  const ProductScreen({
+  final UtilsServices utilsServices = UtilsServices();
+
+  ProductScreen({
     Key? key,
     required this.item,
   }) : super(key: key);
@@ -20,6 +24,7 @@ class ProductScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.all(32),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: const BorderRadius.vertical(
@@ -30,6 +35,73 @@ class ProductScreen extends StatelessWidget {
                     color: Colors.grey.shade600,
                     offset: const Offset(0, 2),
                     blurRadius: 1,
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  //#Nome - Quantidade
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        item.itemName,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.orange,
+                      )
+                    ],
+                  ),
+                  //#Preço
+                  Row(
+                    children: [
+                      Text(
+                        utilsServices.priceToCurrency(item.price),
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.customSwatchColor,
+                        ),
+                      ),
+                      Text(
+                        '/${item.unit}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey.shade500,
+                          // color: CustomColors.customSwatchColor.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  //#Descrição
+                  Expanded(
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        item.description,
+                        textAlign: TextAlign.justify,
+                        style: const TextStyle(
+                          height: 1.5,
+                          letterSpacing: 1.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  //#Bottom
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Botao'),
                   ),
                 ],
               ),
