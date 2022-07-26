d1fa3229440bcc8a7a94271e5bd9bd985f1ae5c3
Osias Miranda <osiasmiranda@gmail.com>
2022-07-25 20:12:02 -0400

    chore: adicionado total e botao concluir pedido

--- lib/src/pages/base/base_screen.dart
+++ lib/src/pages/base/base_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../cart/cart_tab.dart';
 import '../home/home_tab.dart';
 
 class BaseScreen extends StatefulWidget {
@@ -15,30 +16,24 @@ final pageController = PageController();
 class _BaseScreenState extends State<BaseScreen> {
   @override
   Widget build(BuildContext context) {
-    final kTabPages = <Widget>[
-      const Center(
-        child: Icon(Icons.cloud, size: 64.0, color: Colors.teal),
-      ),
-      const Center(
-        child: Icon(Icons.alarm, size: 64.0, color: Colors.red),
-      ),
-      const Center(
-        child: Icon(Icons.forum, size: 64.0, color: Colors.blue),
-      ),
-      const Center(
-        child: Icon(Icons.date_range, size: 64.0, color: Colors.yellow),
-      ),
-    ];
-
     final kBottomNavBarItems = <BottomNavigationBarItem>[
-      const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
       const BottomNavigationBarItem(
-          icon: Icon(Icons.shopping_cart), label: 'Checkout'),
-      const BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Pedidos'),
-      const BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.shopping_cart),
+        label: 'Checkout',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.list),
+        label: 'Pedidos',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: 'Perfil',
+      ),
     ];
-
-    assert(kTabPages.length == kBottomNavBarItems.length);
 
     final bottomNavBar = BottomNavigationBar(
       items: kBottomNavBarItems,
@@ -60,7 +55,7 @@ class _BaseScreenState extends State<BaseScreen> {
         physics: const NeverScrollableScrollPhysics(),
         children: [
           const HomeTab(),
-          Container(color: Colors.blue),
+          CartTab(),
           Container(color: Colors.orange),
           Container(color: Colors.purple),
           // kTabPages[currentTabIndex],
---
+++ lib/src/pages/cart/cart_tab.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import '../../config/custom_colors.dart';
+import '../../services/utils_services.dart';
+
+class CartTab extends StatelessWidget {
+  final UtilsServices utilsServices = UtilsServices();
+
+  CartTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Placeholder(),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  blurRadius: 3,
+                  spreadRadius: 2,
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Total Geral',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  utilsServices.priceToCurrency(50.5),
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: CustomColors.customSwatchColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: CustomColors.customSwatchColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'CONCLUIR PEDIDO',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
